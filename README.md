18633e6a9519c7e34c940334deddf583f64f5ec1
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2023-04-03 21:58:49 +0700

    AntiTrampleFarmland

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# AntiTrampleFarmland
-Farmland will not decay when trampled by any entity
+# General
+**Farmland will not decay when trampled by any entity**
+
+![GIF](https://user-images.githubusercontent.com/60387689/229535076-4e6dc5db-4919-45e4-bc89-fedcea41f598.gif)
+
+# Contact
+[![Discord](https://img.shields.io/discord/986553214889517088?label=discord&color=7289DA&logo=discord)](https://discord.gg/j2X83ujT6c)\
+**You can contact me directly via Discord `NhanAZ#9115`**
---
+++ src/NhanAZ/AntiTrampleFarmland/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NhanAZ\AntiTrampleFarmland;
+
+use pocketmine\event\entity\EntityTrampleFarmlandEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+	protected function onEnable(): void {
+		$this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function onEntityTrampleFarmland(EntityTrampleFarmlandEvent $event): void {
+		$worldName = $event->getEntity()->getWorld()->getDisplayName();
+		$worlds = $this->getConfig()->get("worlds");
+		$isBlacklist = match ($this->getConfig()->get("mode")) {
+			"blacklist" => true,
+			"whitelist" => false
+		};
+		if ($isBlacklist) {
+			if (!in_array($worldName, $worlds)) {
+				$event->cancel();
+			}
+		} else {
+			if (in_array($worldName, $worlds)) {
+				$event->uncancel();
+			}
+		}
+	}
+}
